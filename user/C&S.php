972a6ce950a1c32bb0e99b37c15b6596ff527ffa
LWINNANDAROO\Dell <lwinnandaroo21@gmail.com>
2022-10-03 22:14:05 +0630

    second commit

---
+++ user/C&S.php
@@ -0,0 +1,375 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="./css/C&S.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poly&family=Poppins&family=Roboto&display=swap" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container">
+        <div class=" header"></div>
+        <div>
+            <h3 class="CStitle">Conditions and Services</h3>
+        </div>
+        <div class="box">
+            <!-- <div class="box1"> -->
+            <div class="browse">
+                <h4>Browse A-Z</h4>
+            </div>
+            <ul class="list-inline">
+                <li>
+                    <a href="" class="alpha">A</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">B</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">C</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">D</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">E</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">F</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">G</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">H</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">I</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">J</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">K</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">L</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">M</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">N</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">O</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">P</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">Q</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">R</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">S</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">T</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">U</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">V</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">W</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">X</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">Y</a>
+                </li>
+                <li>
+                    <a href="" class="alpha">Z</a>
+                </li>
+
+            </ul>
+        </div>
+    </div>
+    <!-- </div> -->
+    <div class="diseaseList">
+        <div class="list">
+            <span>A</span>
+            <div class="listDetail">
+                <ul class="diseaseli">
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr class="CShr">
+        <div class="list">
+            <span>B</span>
+            <div class="listDetail">
+                <ul class="diseaseli">
+                    <li>
+                        <a href="">Abdominal</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr class="CShr">
+        <div class="list">
+            <span>C</span>
+            <div class="listDetail">
+                <ul class="diseaseli">
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr class="CShr">
+        <div class="list">
+            <span>D</span>
+            <div class="listDetail">
+                <ul class="diseaseli">
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr class="CShr">
+        <div class="list">
+            <span>E</span>
+            <div class="listDetail">
+                <ul class="diseaseli">
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr class="CShr">
+        <div class="list">
+            <span>F</span>
+            <div class="listDetail">
+                <ul class="diseaseli">
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr class="CShr">
+        <div class="list">
+            <span>G</span>
+            <div class="listDetail">
+                <ul class="diseaseli">
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <hr class="CShr">
+        <div class="list">
+            <span>H</span>
+            <div class="listDetail">
+                <ul class="diseaseli">
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                    <li>
+                        <a href="">Abdominal Aortic Aneurysm</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    </div>
+    <div class=" footer "></div>
+    </div>
+</body>
+
+</html>
