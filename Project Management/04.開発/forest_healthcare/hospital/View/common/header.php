1aff720fb1709fadfd3a64fd7190598b51e400be
May Moe <maymoeyamoun@gmail.com>
2022-10-08 22:16:16 +0630

    maymoe

--- Project Management/04.開発/forest_healthcare/hospital/View/common/header.php
+++ Project Management/04.開発/forest_healthcare/hospital/View/common/header.php
@@ -1,109 +1,107 @@
+<div class="wrapper">
 
-    <div class="wrapper">
-
-        <header class="main-header">
-            <!-- Logo -->
-            <a href="index2.html" class="logo"><img src="./image/punhlainglogo .png" id="logo" alt=""><b>HOSPITAL</b></a>
-            <!-- Header Navbar: style can be found in header.less -->
-            <nav class="navbar navbar-static-top" role="navigation">
-                <!-- Sidebar toggle button-->
-                <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-                    <span class="sr-only">Toggle navigation</span>
-                </a>
-                <div class="navbar-custom-menu">
-                    <ul class="nav navbar-nav">
-                        <!-- Messages: style can be found in dropdown-->
-                        <li class="dropdown messages-menu">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <i class="fa fa-envelope-o"></i>
-                                <span class="label label-success">4</span>
-                            </a>
-
-                        </li>
-                        <!-- User Account: style can be found in dropdown -->
-                        <li class="dropdown user user-menu">
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                                <img src="./image/Hospitalphoto.png" class="user-image" alt="User Image" />
-
-                            </a>
-                            <ul class="dropdown-menu">
-                                <!-- User image -->
-                                <li class="user-header">
-                                    <img src="./image/Hospitalphoto.png" class="img-circle" alt="User Image" />
-                                    <p>
-
-                                    </p>
-                                </li>
-                                <!-- Menu Body -->
-
-                                </li>
-
-                            </ul>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        </header>
-        <!-- Left side column. contains the logo and sidebar -->
-        <aside class="main-sidebar">
-            <!-- sidebar: style can be found in sidebar.less -->
-            <section class="sidebar">
-                <!-- Sidebar user panel -->
-                <div class="user-panel">
-                    <div class="pull-left image">
-                        <img src="./image/Hospitalphoto.png" class="img-circle" alt="User Image" />
-                    </div>
-                    <div class="pull-left info">
-                        <p>Welcome</p>
-
-                        <p>Pun Hlaing Hospital</p>
-                    </div>
-                </div>
-                <!-- sidebar menu: : style can be found in sidebar.less -->
-                <ul class="sidebar-menu">
-                    <li class="active treeview">
-                        <a href="#">Dashboard</a>
-                    </li>
-                    <li class="treeview">
-                        <a href="#">
-                            <i class="fa fa-pie-chart"></i>
-                            <span>Doctor</span>
-                            <i class="fa fa-angle-left pull-right"></i>
+    <header class="main-header">
+        <!-- Logo -->
+        <a href="index2.html" class="logo main-header1"><img src="./image/punhlainglogo .png" id="logo" alt=""><b>HOSPITAL</b></a>
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top main-header2" role="navigation">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <!-- Messages: style can be found in dropdown-->
+                    <li class="dropdown messages-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="fa fa fa-envelope-o"></i>
+                            <span class="label label-success">4</span>
                         </a>
-                        <ul class="treeview-menu">
-                            <li><a href="Hospitaladddoctor.html"><i class="fa fa-circle-o"></i>Add Doctor List</a></li>
-                            <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i>Doctor list</a></li>
-                            <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i>Schedule list</a></li>
-                        </ul>
-                    </li>
-                    <li class="treeview">
-                    <li><a href=""><i class="fa fa-book"></i> Appointment</a></li>
 
                     </li>
-                    <li>
-                        <a href="pages/mailbox/mailbox.html">
-                            <i class="fa fa-envelope"></i> <span>Mail</span>
-                            <small class="label pull-right bg-yellow">12</small>
+                    <!-- User Account: style can be found in dropdown -->
+                    <li class="dropdown user user-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <img src="./image/Hospitalphoto.png" class="user-image" alt="User Image" />
+
                         </a>
+                        <ul class="dropdown-menu">
+                            <!-- User image -->
+                            <li class="user-header">
+                                <img src="./image/Hospitalphoto.png" class="img-circle" alt="User Image" />
+                                <p>
+
+                                </p>
+                            </li>
+                            <!-- Menu Body -->
+
                     </li>
-                    <li class="treeview">
-                        <a href="#">
-                            <i class="fa fa-folder"></i> <span>Account Setting</span>
-                            <i class="fa fa-angle-left pull-right"></i>
-                        </a>
-                    </li>
+
                 </ul>
-            </section>
-            <!-- /.sidebar -->
-        </aside>
+                </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!-- Left side column. contains the logo and sidebar -->
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+            <!-- Sidebar user panel -->
+            <div class="user-panel">
+                <div class="pull-left image">
+                    <img src="./image/Hospitalphoto.png" class="img-circle" alt="User Image" />
+                </div>
+                <div class="pull-left info">
+                    <p>Welcome</p>
+                    <p>Pun Hlaing Hospital</p>
+                </div>
+            </div>
+            <!-- sidebar menu: : style can be found in sidebar.less -->
+            <ul class="sidebar-menu">
+                <li class="active treeview">
+                    <a href="#">Dashboard</a>
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-pie-chart"></i>
+                        <span>Doctor</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li><a href="Hospitaladddoctor.html"><i class="fa fa-circle-o"></i>Add Doctor List</a></li>
+                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i>Doctor list</a></li>
+                        <li><a href="pages/charts/flot.html"><i class="fa fa-circle-o"></i>Schedule list</a></li>
+                    </ul>
+                </li>
+                <li class="treeview">
+                <li><a href=""><i class="fa fa-book"></i> Appointment</a></li>
 
-        <!-- Right side column. Contains the navbar and content of the page -->
-        <div class="content-wrapper">
-            <!-- Content Header (Page header) -->
-            <section class="content-header">
-                <h1><i class="fa fa-dashboard"></i>&nbsp;&nbsp;Dashboard</h1>
-                <div><small>Dashboard Features</small></div>
-                <ol class="breadcrumb">
-                    <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                    <li class="active">Dashboard</li>
-                </ol>
+                </li>
+                <li>
+                    <a href="pages/mailbox/mailbox.html">
+                        <i class="fa fa-envelope"></i> <span>Mail</span>
+                        <small class="label pull-right bg-yellow">12</small>
+                    </a>
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-folder"></i> <span>Account Setting</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                </li>
+            </ul>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Right side column. Contains the navbar and content of the page -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <h1><i class="fa fa-dashboard"></i>&nbsp;&nbsp;Dashboard</h1>
+            <div><small>Dashboard Features</small></div>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Dashboard</li>
+            </ol>
