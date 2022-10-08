93316f8da0f06e179eaacefe0f374829fca10b06
LWINNANDAROO\Dell <lwinnandaroo21@gmail.com>
2022-10-08 19:42:10 +0630

    nandar commit

--- Project Management/04.開発/forest_healthcare/admin/View/adminContactList.php
+++ Project Management/04.開発/forest_healthcare/admin/View/adminContactList.php
@@ -6,97 +6,101 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poly&family=Poppins&family=Roboto&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="./resources/css/adminContactList.css?v=" <?= time() ?>>
+    <!-- Bootstrap -->
+    <link href="./resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- FontAwesome 4.3.0 -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons 2.0.0 -->
+    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="./resources/css/Admin.min.css" rel="stylesheet" type="text/css" />
 </head>
 
-<body>
+<body class="skin-blue">
+    <!-- Header -->
+    <?php include("common/header.php"); ?>
+
     <div class="container">
-    <div class="row mt-5 mb-3 mailList">
-            <!-- <div class="col-md-3 col-sm-2 col-2"></div> -->
-            <div class="col-md-12 col-sm-8 col-8 adminContactReply-col adminReplytitle">Contact Us Mail List</div>
-            <!-- <div class="col-md-3 col-sm-2 col-2"></div> -->
+        <div class="row mailList">
+            <div class="col-md-2 col-sm-2 col-2"></div>
+            <div class="col-md-6 col-sm-6 col-6">Contact Us Mail List</div>
+            <div class="col-md-2 col-sm-2 col-2"></div>
         </div>
         <div class="row">
-            <!-- <div class="col-sm-3 col-2"></div> -->
-            <div class="col-sm-4 col-9 adminContactListCard">
-                <div class="card adminContactListCardBg">
-                    <div class="card-body">
-                        <div class="row mb-2">
-                            <div class="col-sm-8 contactListTitle">
-                                <div class="">Aung Aung</div>
-                                <div class="">aung@gmail.com</div>
-                                <div class="">09-2150776</div>
-                            </div>
-                            <div class="col-sm-4">
-                                <a href="#" class="btn adminContactListbtn">Reply</a>
-                            </div>
+            <!-- <div class="col-md-1 col-sm-3 col-2"></div> -->
+            <div class="card adminContactListCardBg">
+                <div class="card-body cardBody">
+                    <div class="row cardRow">
+                        <div class="col-sm-8 contactListTitle">
+                            <div class="">Aung Aung</div>
+                            <div class="">aung@gmail.com</div>
+                            <div class="">09-2150776</div>
                         </div>
-                        <p class="adminContactListCardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat earum dolor cumque, a ipsam modi iste veritatis assumenda harum totam, animi rerum omnis nam alias minus? A, cum sint.</p>
+                        <div class="col-sm-4">
+                            <a href="#" class="btn adminContactListbtn">Reply</a>
+                        </div>
                     </div>
+                    <p class="adminContactListCardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat earum dolor cumque, a ipsam modi iste veritatis assumenda harum totam, animi rerum omnis nam alias minus? A, cum sint.</p>
                 </div>
             </div>
-            <div class="col-sm-4 col-8 adminContactListCard">
-                <div class="card adminContactListCardBg">
-                    <div class="card-body">
-                        <div class="row mb-2">
-                            <div class="col-sm-8 contactListTitle">
-                                <div class="">Aung Aung</div>
-                                <div class="">aung@gmail.com</div>
-                                <div class="">09-2150776</div>
-                            </div>
-                            <div class="col-sm-4">
-                                <a href="#" class="btn adminContactListbtn">Reply</a>
-                            </div>
+            <div class="card adminContactListCardBg">
+                <div class="card-body cardBody">
+                    <div class="row mb-2 cardRow">
+                        <div class="col-sm-8 contactListTitle">
+                            <div class="">Aung Aung</div>
+                            <div class="">aung@gmail.com</div>
+                            <div class="">09-2150776</div>
                         </div>
-                        <p class="adminContactListCardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat earum dolor cumque, a ipsam modi iste veritatis assumenda harum totam, animi rerum omnis nam alias minus? A, cum sint.</p>
+                        <div class="col-sm-4">
+                            <a href="#" class="btn adminContactListbtn">Reply</a>
+                        </div>
                     </div>
+                    <p class="adminContactListCardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat earum dolor cumque, a ipsam modi iste veritatis assumenda harum totam, animi rerum omnis nam alias minus? A, cum sint.</p>
                 </div>
             </div>
-            <div class="col-sm-4 col-9 adminContactListCard">
-                <div class="card adminContactListCardBg">
-                    <div class="card-body">
-                        <div class="row mb-2">
-                            <div class="col-sm-8 contactListTitle">
-                                <div class="">Aung Aung</div>
-                                <div class="">aung@gmail.com</div>
-                                <div class="">09-2150776</div>
-                            </div>
-                            <div class="col-sm-4">
-                                <a href="#" class="btn adminContactListbtn">Reply</a>
-                            </div>
+            <div class="card adminContactListCardBg">
+                <div class="card-body cardBody">
+                    <div class="row mb-2 cardRow">
+                        <div class="col-sm-8 contactListTitle">
+                            <div class="">Aung Aung</div>
+                            <div class="">aung@gmail.com</div>
+                            <div class="">09-2150776</div>
                         </div>
-                        <p class="adminContactListCardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat earum dolor cumque, a ipsam modi iste veritatis assumenda harum totam, animi rerum omnis nam alias minus? A, cum sint.</p>
+                        <div class="col-sm-4">
+                            <a href="#" class="btn adminContactListbtn">Reply</a>
+                        </div>
                     </div>
+                    <p class="adminContactListCardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat earum dolor cumque, a ipsam modi iste veritatis assumenda harum totam, animi rerum omnis nam alias minus? A, cum sint.</p>
                 </div>
             </div>
-            <div class="col-sm-4 col-8 adminContactListCard">
-                <div class="card adminContactListCardBg">
-                    <div class="card-body">
-                        <div class="row mb-2">
-                            <div class="col-sm-8 contactListTitle">
-                                <div class="">Aung Aung</div>
-                                <div class="">aung@gmail.com</div>
-                                <div class="">09-2150776</div>
-                            </div>
-                            <div class="col-sm-4">
-                                <a href="#" class="btn adminContactListbtn">Reply</a>
-                            </div>
+            <div class="card adminContactListCardBg">
+                <div class="card-body cardBody">
+                    <div class="row mb-2 cardRow">
+                        <div class="col-sm-8 contactListTitle">
+                            <div class="">Aung Aung</div>
+                            <div class="">aung@gmail.com</div>
+                            <div class="">09-2150776</div>
                         </div>
-                        <p class="adminContactListCardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat earum dolor cumque, a ipsam modi iste veritatis assumenda harum totam, animi rerum omnis nam alias minus? A, cum sint.</p>
+                        <div class="col-sm-4">
+                            <a href="#" class="btn adminContactListbtn">Reply</a>
+                        </div>
                     </div>
+                    <p class="adminContactListCardText">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate quaerat earum dolor cumque, a ipsam modi iste veritatis assumenda harum totam, animi rerum omnis nam alias minus? A, cum sint.</p>
                 </div>
             </div>
-            
-            <!-- <div class="col-sm-1 col-1"></div> -->
+            <!-- <div class="col-md-1 col-sm-1 col-1"></div> -->
         </div>
 
     </div>
+    <!-- Footer -->
+    <?php include("common/footer.php"); ?>
 </body>
 
 </html>
--- Project Management/04.開発/forest_healthcare/admin/View/adminP&Plist.php
+++ Project Management/04.開発/forest_healthcare/admin/View/adminP&Plist.php
@@ -6,16 +6,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
     <link rel="stylesheet" href="./resources/css/adminP&Plist.css?v="<?=time()?>>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Poly&family=Poppins&family=Roboto&display=swap" rel="stylesheet">
+ <!-- Bootstrap -->
+ <link href="./resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <!-- FontAwesome 4.3.0 -->
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- Ionicons 2.0.0 -->
+    <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
+    <!-- Theme style -->
+    <link href="./resources/css/Admin.min.css" rel="stylesheet" type="text/css" />
+
 </head>
 
-<body>
+<body class="skin-blue">
+    <!-- Header -->
+    <?php include("common/header.php"); ?>
     <div class="container">
         <div class="row mainadminPPaddtitle">
             <div class="col-8 adminPPaddtitle">Your Privacy & Policy List</div>
@@ -29,8 +40,8 @@
             </div>
         </div>
         <div class="row">
-            <!-- <div class="col-md-2 col-sm-2 "></div> -->
-            <div class="col-md-8 col-sm-10 mx-auto  table-responsive">
+            <div class="col-md-1 col-sm-1 "></div>
+            <div class="col-md-8 col-sm-10 table-responsive">
             <table class="table">
                 <thead class="adminPPlistheaderbg">
                     <tr>
@@ -47,7 +58,7 @@
                     <tr>
                         <th scope="row">1</th>
                         <td>Question</td>
-                        <td class="nandar"><?php echo substr("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis corporis aspernatur, nostrum facere voluptas exercitationem illo, saepe libero sit maxime repellendus illum! Possimus reprehenderit similique quae ipsa assumenda, nostrum aliquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat eum beatae fugit. Possimus consectetur saepe earum! Eos iusto numquam error commodi minima, exercitationem voluptatem, animi sapiente mollitia, sint odit ab?",0,50)?>...  </td>
+                        <td><?php echo substr("Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis corporis aspernatur, nostrum facere voluptas exercitationem illo, saepe libero sit maxime repellendus illum! Possimus reprehenderit similique quae ipsa assumenda, nostrum aliquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat eum beatae fugit. Possimus consectetur saepe earum! Eos iusto numquam error commodi minima, exercitationem voluptatem, animi sapiente mollitia, sint odit ab?",0,50)?>...  </td>
                         <td>
                             <button class="btn"><i class="fa-solid fa-pen-to-square adminPPeditIcon"></i></button>
                             <button class="btn"><i class="fa-solid fa-trash-can adminPPdeleteIcon"></i></button>
@@ -96,70 +107,7 @@
             </div>
             <!-- <div class="col-md-2 col-sm-2 "></div> -->
         </div>
-        <!-- <div class="table-responsive">
-            <table class="table">
-                <thead class="adminPPlistheaderbg">
-                    <tr>
-                        <th scope="col">No</th>
-                        <th scope="col">Question</th>
-                        <th scope="col">Answer</th>
-                        <th scope="col">Active</th>
-                        <th scope="col">Question</th>
-                        <th scope="col">Answer</th>
-                        <th scope="col">Active</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Question</td>
-                        <td>Answer</td>
-                        <td>
-                            <button class="btn"><i class="fa-solid fa-pen-to-square adminPPeditIcon"></i></button>
-                            <button class="btn"><i class="fa-solid fa-trash-can adminPPdeleteIcon"></i></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Question</td>
-                        <td>Answer</td>
-                        <td>
-                            <button class="btn"><i class="fa-solid fa-pen-to-square adminPPeditIcon"></i></button>
-                            <button class="btn"><i class="fa-solid fa-trash-can adminPPdeleteIcon"></i></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td>Question</td>
-                        <td>Answer</td>
-                        <td>
-                            <button class="btn"><i class="fa-solid fa-pen-to-square adminPPeditIcon"></i></button>
-                            <button class="btn"><i class="fa-solid fa-trash-can adminPPdeleteIcon"></i></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">4</th>
-                        <td>Question</td>
-                        <td>Answer</td>
-                        <td>
-                            <button class="btn"><i class="fa-solid fa-pen-to-square adminPPeditIcon"></i></button>
-                            <button class="btn"><i class="fa-solid fa-trash-can adminPPdeleteIcon"></i></button>
-                        </td>
-                    </tr>
-                    <tr>
-                        <th scope="row">5</th>
-                        <td>Question</td>
-                        <td>Answer</td>
-                        <td>
-                            <button class="btn"><i class="fa-solid fa-pen-to-square adminPPeditIcon"></i></button>
-                            <button class="btn"><i class="fa-solid fa-trash-can adminPPdeleteIcon"></i></button>
-                        </td>
-                    </tr>
-
-                </tbody>
-            </table>
-        </div> -->
-
+        
         <div class="row">
             <div class="col-md-10 col-sm-5"></div>
             <div class="col-md-2 col-sm-5">
@@ -184,6 +132,8 @@
                 </nav>
             </div>
         </div>
+        <!-- Footer -->
+    <?php include("common/footer.php"); ?>
 </body>
 
 </html>
