b325f485af7448043d442a28c05b2683ad64d70c
May Moe <maymoeyamoun@gmail.com>
2022-10-09 17:48:59 +0630

    Admin/Hospital Dashboard Final Commit

--- Project Management/04.開発/forest_healthcare/admin/View/common/header.php
+++ Project Management/04.開発/forest_healthcare/admin/View/common/header.php
@@ -1,180 +1,180 @@
 <div class="wrapper">
 
-<header class="main-header">
-    <!-- Logo -->
-    <a href="" class="logo"><img src="./image/logoAdmin.png" id="logo" alt=""><b>ADMIN</b></a>
-    <!-- Header Navbar: style can be found in header.less -->
-    <nav class="navbar navbar-static-top" role="navigation">
-        <!-- Sidebar toggle button-->
-        <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
-            <span class="sr-only">Toggle navigation</span>
-        </a>
-        <div class="navbar-custom-menu">
-            <ul class="nav navbar-nav">
-                <!-- Messages: style can be found in dropdown.less-->
-                <li class="dropdown messages-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-envelope-o"></i>
-                        <span class="label label-success">4</span>
+    <header class="main-header">
+        <!-- Logo -->
+        <a href="" class="logo"><img src="./image/logoAdmin.png" id="logo" alt=""><b>ADMIN</b></a>
+        <!-- Header Navbar: style can be found in header.less -->
+        <nav class="navbar navbar-static-top" role="navigation">
+            <!-- Sidebar toggle button-->
+            <a href="#" class="sidebar-toggle" data-toggle="offcanvas" role="button">
+                <span class="sr-only">Toggle navigation</span>
+            </a>
+            <div class="navbar-custom-menu">
+                <ul class="nav navbar-nav">
+                    <!-- Messages: style can be found in dropdown.less-->
+                    <li class="dropdown messages-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="fa fa-envelope-o"></i>
+                            <span class="label label-success">4</span>
+                        </a>
+                    </li>
+                    <!-- User Account: style can be found in dropdown.less -->
+                    <li class="dropdown user user-menu">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+                            <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <!-- User image -->
+                            <li class="user-header">
+                                <img src="./image/user2-160x160.jpg" class="img-circle" alt="User Image" />
+                                <p>
+                                    Alexander Pierce
+                                </p>
+                            </li>
+                            <!-- Menu Body -->
+                            <li class="user-body">
+                                <div class="col-xs-4 text-center">
+                                    <a href="#"></a>
+                                </div>
+                                <div class="col-xs-4 text-center">
+                                    <a href="#"></a>
+                                </div>
+                                <div class="col-xs-4 text-center">
+                                    <a href="#">Friends</a>
+                                </div>
+                            </li>
+                            <!-- Menu Footer-->
+                            <li class="user-footer">
+                                <div class="pull-left">
+                                    <a href="#" class="btn btn-default btn-flat">Profile</a>
+                                </div>
+                                <div class="pull-right">
+                                    <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </header>
+    <!-- Left side column. contains the logo and sidebar -->
+    <aside class="main-sidebar">
+        <!-- sidebar: style can be found in sidebar.less -->
+        <section class="sidebar">
+            <!-- Sidebar user panel -->
+            <div class="user-panel">
+                <div class="pull-left image">
+                    <img src="./image/user2-160x160.jpg" class="img-circle" alt="User Image" />
+                </div>
+                <div class="pull-left info">
+                    <p>Welcome</p>
+                    <p>Alexander Pierce</p>
+                </div>
+            </div>
+            <!-- /.search form -->
+            <!-- sidebar menu: : style can be found in sidebar.less -->
+            <ul class="sidebar-menu">
+                <li class="active treeview">
+                    <a href="#">
+                        <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                     </a>
                 </li>
-                <!-- User Account: style can be found in dropdown.less -->
-                <li class="dropdown user user-menu">
-                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                        <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-user-md" aria-hidden="true"></i>
+                        <span>Doctor</span>
+                        <i class="fa fa-angle-left pull-right"></i>
                     </a>
-                    <ul class="dropdown-menu">
-                        <!-- User image -->
-                        <li class="user-header">
-                            <img src="./image/user2-160x160.jpg" class="img-circle" alt="User Image" />
-                            <p>
-                                Alexander Pierce
-                            </p>
+                    <ul class="treeview-menu">
+                        <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Doctor List</a></li>
+                        <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Schedule List</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="pages/widgets.html">
+                        <i class="fa fa-hospital-o" aria-hidden="true"></i>
+                        <span>Hospital</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Add Hospital</a></li>
+                        <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Hospital List</a></li>
+                    </ul>
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-calendar-o" aria-hidden="true"></i>
+                        <span>Appointment</span>
+                    </a>
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-edit"></i>
+                        <span>Post</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class="active"><a href="">Condition & Service</a>
+                            <ul class="treeview-menu">
+                                <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Add Disease</a></li>
+                                <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Disease List</a></li>
+                            </ul>
                         </li>
-                        <!-- Menu Body -->
-                        <li class="user-body">
-                            <div class="col-xs-4 text-center">
-                                <a href="#"></a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#"></a>
-                            </div>
-                            <div class="col-xs-4 text-center">
-                                <a href="#">Friends</a>
-                            </div>
+                        <li class="active"><a href="">Health Knowledge</a>
+                            <ul class="treeview-menu">
+                                <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Add Article</a></li>
+                                <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Article List</a></li>
+                            </ul>
                         </li>
-                        <!-- Menu Footer-->
-                        <li class="user-footer">
-                            <div class="pull-left">
-                                <a href="#" class="btn btn-default btn-flat">Profile</a>
-                            </div>
-                            <div class="pull-right">
-                                <a href="#" class="btn btn-default btn-flat">Sign out</a>
-                            </div>
+                        <li class="active"><a href="">Mental Health</a>
+                            <ul class="treeview-menu">
+                                <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Add Viedo</a></li>
+                                <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Viedo List</a></li>
+                            </ul>
                         </li>
                     </ul>
                 </li>
-            </ul>
-        </div>
-    </nav>
-</header>
-<!-- Left side column. contains the logo and sidebar -->
-<aside class="main-sidebar">
-    <!-- sidebar: style can be found in sidebar.less -->
-    <section class="sidebar">
-        <!-- Sidebar user panel -->
-        <div class="user-panel">
-            <div class="pull-left image">
-                <img src="./image/user2-160x160.jpg" class="img-circle" alt="User Image" />
-            </div>
-            <div class="pull-left info">
-                <p>Welcome</p>
-                <p>Alexander Pierce</p>
-            </div>
-        </div>
-        <!-- /.search form -->
-        <!-- sidebar menu: : style can be found in sidebar.less -->
-        <ul class="sidebar-menu">
-            <li class="active treeview">
-                <a href="#">
-                    <i class="fa fa-dashboard"></i> <span>Dashboard</span>
-                </a>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-user-md" aria-hidden="true"></i>
-                    <span>Doctor</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Doctor List</a></li>
-                    <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Schedule List</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="pages/widgets.html">
-                    <i class="fa fa-hospital-o" aria-hidden="true"></i>
-                    <span>Hospital</span> 
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Add Hospital</a></li>
-                    <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Hospital List</a></li>
-                </ul>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-calendar-o" aria-hidden="true"></i>
-                    <span>Appointment</span>
-                </a>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-edit"></i>
-                    <span>Post</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li class="active"><a href="">Condition & Service</a>
-                        <ul class="treeview-menu">
-                            <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Add Disease</a></li>
-                            <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Disease List</a></li>
-                        </ul>
-                    </li>
-                    <li class="active"><a href="">Health Knowledge</a>
-                        <ul class="treeview-menu">
-                            <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Add Article</a></li>
-                            <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Article List</a></li>
-                        </ul>
-                    </li>
-                    <li class="active"><a href="">Mental Health</a>
-                        <ul class="treeview-menu">
-                            <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Add Viedo</a></li>
-                            <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Viedo List</a></li>
-                        </ul>
-                    </li>
-                </ul>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-users" aria-hidden="true"></i> <span>User</span>
-                </a>
-            </li>
-            <li>
-                <a href="">
-                    <i class="fa fa-envelope"></i> <span>Mail</span>
-                    <small class="label pull-right bg-yellow">12</small>
-                </a>
-            </li>
-            <li class="treeview">
-                <a href="#">
-                    <i class="fa fa-cog" aria-hidden="true"></i><span>Setting</span>
-                    <i class="fa fa-angle-left pull-right"></i>
-                </a>
-                <ul class="treeview-menu">
-                    <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Privacy & Policy</a></li>
-                    <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Contact Us</a></li>
-                    <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>About Us</a></li>
-                    <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Account Setting</a></li>
-                </ul>
-            </li>
-        
-        </ul>
-    </section>
-    <!-- /.sidebar -->
-</aside>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-users" aria-hidden="true"></i> <span>User</span>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <i class="fa fa-envelope"></i> <span>Mail</span>
+                        <small class="label pull-right bg-yellow">12</small>
+                    </a>
+                </li>
+                <li class="treeview">
+                    <a href="#">
+                        <i class="fa fa-cog" aria-hidden="true"></i><span>Setting</span>
+                        <i class="fa fa-angle-left pull-right"></i>
+                    </a>
+                    <ul class="treeview-menu">
+                        <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Privacy & Policy</a></li>
+                        <li><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Contact Us</a></li>
+                        <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>About Us</a></li>
+                        <li class="active"><a href=""><i class="fa fa-circle" aria-hidden="true"></i>Account Setting</a></li>
+                    </ul>
+                </li>
 
-<!-- Right side column. Contains the navbar and content of the page -->
-<div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <i class="fa fa-dashboard icon"></i>
-        <span>
-            <span class="content-header-text1">Dashboard</span>
-            <p class="content-header-text2"><small>Dashboard Features</small></p>
-        </span>
-        <ol class="breadcrumb">
-            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-            <li class="active">Dashboard</li>
-        </ol>
+            </ul>
+        </section>
+        <!-- /.sidebar -->
+    </aside>
+
+    <!-- Right side column. Contains the navbar and content of the page -->
+    <div class="content-wrapper">
+        <!-- Content Header (Page header) -->
+        <section class="content-header">
+            <i class="fa fa-dashboard icon"></i>
+            <span>
+                <span class="content-header-text1">Dashboard</span>
+                <p class="content-header-text2"><small>Dashboard Features</small></p>
+            </span>
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li class="active">Dashboard</li>
+            </ol>
         </section>
--- Project Management/04.開発/forest_healthcare/hospital/View/hAddDoctor.php
+++ Project Management/04.開発/forest_healthcare/hospital/View/hAddDoctor.php
@@ -10,7 +10,6 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
     <link rel="stylesheet" href="./resources/css/hAddDoctor.css?v=" <?= time() ?>>
     <link rel="stylesheet" href="./resources/css/main.css?v=" <?= time() ?>>
-    <link rel="stylesheet" href="./resources/css/adminContact.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -21,19 +20,33 @@
     <link href="./resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <!-- FontAwesome 4.3.0 -->
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
+    <script src="https://kit.fontawesome.com/5053738b7f.js" crossorigin="anonymous"></script>
     <!-- Ionicons 2.0.0 -->
     <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
     <link href="./resources/css/Admin.min.css" rel="stylesheet" type="text/css" />
+    <link href="./resources/css/_all-skins.min.css" rel="stylesheet" type="text/css" />
+
 </head>
 
-<body>
+<body class="skin-blue">
 
     <!-- Header -->
     <?php include("common/header.php"); ?>
+    <section class="content-header">
+    <i class="fa fa-user-md icon" aria-hidden="true"></i>
 
-
-
+        <span>
+            <span class="content-header-text1">Doctor</span>
+            <p class="content-header-text2"><small>Add Doctor</small></p>
+        </span>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Doctor</li>
+        </ol>
+    </section>
+    <!-- Header -->
     <div class="container">
         <div class="row">
             <div class="col-md-1 col-sm-1 col-2"></div>
@@ -49,80 +62,79 @@
                     </div>
                     <div class="form-group">
                         <div class="col-md-12">
-                                <a href="#"> Department</a>
-                                <ul class="dropdown">
-                                    <li><a href="#">Neurology</a></li>
-                                    <li><a href="#">Neurology</a></li>
-                                    <li><a href="#">Neurology</a></li>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-
-                    </div>
-                    <div class="form-group">
-                        <!-- <label for="username" class="control-label col-md-4">Username</label> -->
-                        <div class="col-md-12">
-                            <h4><b>Expert In</b></h4>
-
-                            <input type="email" class="form-control doctorInput" placeholder="Expert In">
+                            <a href="#"> Department</a>
+                            <ul class="dropdown">
+                                <li><a href="#">Neurology</a></li>
+                                <li><a href="#">Neurology</a></li>
+                                <li><a href="#">Neurology</a></li>
+                                </li>
+                            </ul>
                         </div>
                     </div>
-                    <div class="form-group">
-                        <div class="col-md-12">
-                            <h4><b>Mobile No</b></h4>
-
-                            <input type="text" class="form-control doctorInput" placeholder="Mobile No">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-12">
-                            <h4><b>Day</b></h4>
-
-                            <input type="date" class="form-control doctorInput" placeholder="Day">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-12">
-                            <h4><b>Start Time</b></h4>
-
-                            <input type="text" class="form-control doctorInput" placeholder="Start Time">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="col-md-12">
-                            <h4><b>End Time</b></h4>
-
-                            <input type="text" class="form-control doctorInput" placeholder="End Time">
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <!-- <label for="username" class="control-label col-md-4">Username</label> -->
-                        <div class="col-md-12">
-                            <h4><b>Address</b></h4>
-
-                            <textarea type="text" class="form-control adminContactTextarea" placeholder="Address"></textarea>
-                        </div>
-                    </div>
-                    <div class="form-group">
-                        <div class="row">
-                            <div class="col-md-9"></div>
-                            <div class="col-md-3">
-                                <input type="button" value="Save" class="adminContactbtn">
-                            </div>
-                        </div>
-                    </div>
-                </form>
 
             </div>
-            <div class="col-md-1 col-sm-1 col-2"></div>
+            <div class="form-group">
+                <!-- <label for="username" class="control-label col-md-4">Username</label> -->
+                <div class="col-md-12">
+                    <h4><b>Expert In</b></h4>
+
+                    <input type="email" class="form-control doctorInput" placeholder="Expert In">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-12">
+                    <h4><b>Mobile No</b></h4>
+
+                    <input type="text" class="form-control doctorInput" placeholder="Mobile No">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-12">
+                    <h4><b>Day</b></h4>
+
+                    <input type="date" class="form-control doctorInput" placeholder="Day">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-12">
+                    <h4><b>Start Time</b></h4>
+
+                    <input type="text" class="form-control doctorInput" placeholder="Start Time">
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="col-md-12">
+                    <h4><b>End Time</b></h4>
+
+                    <input type="text" class="form-control doctorInput" placeholder="End Time">
+                </div>
+            </div>
+            <div class="form-group">
+                <!-- <label for="username" class="control-label col-md-4">Username</label> -->
+                <div class="col-md-12">
+                    <h4><b>Address</b></h4>
+
+                    <textarea type="text" class="form-control adminContactTextarea" placeholder="Address"></textarea>
+                </div>
+            </div>
+            <div class="form-group">
+                <div class="row">
+                    <div class="col-md-9"></div>
+                    <div class="col-md-3">
+                        <input type="button" value="Save" class="adminContactbtn">
+                    </div>
+                </div>
+            </div>
+            </form>
+
+        </div>
+        <div class="col-md-1 col-sm-1 col-2"></div>
 
 
+        <!-- Footer -->
+        <?php include("common/footer.php"); ?>
 
-            <!-- Footer -->
-            <?php include("common/footer.php"); ?>
-
-            <script src="./resources/js/adminAboutUs.js"></script>
+        <script src="./resources/js/adminAboutUs.js"></script>
 
 </body>
 
