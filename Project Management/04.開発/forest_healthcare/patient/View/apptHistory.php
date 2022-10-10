42bd2ad0854394bd89b260109ba318c9c591c2bc
May Moe <maymoeyamoun@gmail.com>
2022-10-10 23:52:34 +0630

    maymoe

--- Project Management/04.開発/forest_healthcare/patient/View/apptHistory.php
+++ Project Management/04.開発/forest_healthcare/patient/View/apptHistory.php
@@ -29,25 +29,105 @@
     <?php include("./common/top.php"); ?>
     <!-- Header -->
 
-    <div class="row">
-        <div>
-            <div class="col-lg-6 menubox">
-                <div class="row">
-                    <div class="col-2 ">
-                        <div><i class="fa-solid fa-user"></i></div>
-                        <div><i class="fa-regular fa-calendar-check"></i></div>
-                        <div><i class="fa-regular fa-user-gear"></i></div>
-                        <div><i class="fa-solid fa-right-from-bracket"></i></div>
-                    </div>
-                    <div class="col-10">
-                        <div>My Profile</div>
-                        <div>Appointment History</div>
-                        <div>Account Setting</div>
-                        <div>Logout</div>
-                    </div>
-
+    <div class="row mt-3">
+        <div class="col-lg-3 ms-5">
+            <div class="row  menubox">
+                <div class="col-2 offset-2">
+                    <div><i class="fa-solid fa-user fs-5 mt-5"></i></div>
+                    <div><i class="fa-regular fa-calendar-check fs-5 mt-5"></i></div>
+                    <div><i class="fa-solid fa-user-gear fs-5 mt-5"></i></div>
+                    <div><i class="fa-solid fa-right-from-bracket fs-5 mt-5"></i></div>
                 </div>
-                    <!-- <button class="btn btn-primary d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
+                <div class="col-6 fw-bold">
+                    <div class=" mt-5"><a href="" class="text-dark">My Profile</a></div>
+                    <div class=" mt-5"><a href="" class="text-dark">Appointment History</a></div>
+                    <div class=" mt-5"><a href="" class="text-dark">Account Setting</a></div>
+                    <div class="mt-5 mb-5"> <a href="" class="text-dark">Logout</a></div>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-8">
+            <table class="table">
+                <thead>
+                    <tr class="title">
+                        <th scope="col">No</th>
+                        <th scope="col">Name</th>
+                        <th scope="col">Doctor</th>
+                        <th scope="col">Date&Time</th>
+                        <th scope="col">Details</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">QR</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Dr.John</td>
+                        <td>9AM-11AM(SUN)</td>
+                        <td><button class="btnview">View</button></td>
+                        <td>Approved</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Mark</td>
+                        <td>Dr.John</td>
+                        <td>9AM-11AM(SUN)</td>
+                        <td><button class="btnview">View</button></td>
+                        <td>Approved</td>
+                        <td></td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Mark</td>
+                        <td>Dr.John</td>
+                        <td>9AM-11AM(SUN)</td>
+                        <td><button class="btnview">View</button></td>
+                        <td>Canceled</td>
+                        <td>---</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Mark</td>
+                        <td>Dr.John</td>
+                        <td>9AM-11AM(SUN)</td>
+                        <td><button class="btnview">View</button></td>
+                        <td>Canceled</td>
+                        <td>---</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Mark</td>
+                        <td>Dr.John</td>
+                        <td>9AM-11AM(SUN)</td>
+                        <td><button class="btnview">View</button></td>
+                        <td>Pending</td>
+                        <td>---</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Mark</td>
+                        <td>Dr.John</td>
+                        <td>9AM-11AM(SUN)</td>
+                        <td><button class="btnview">View</button></td>
+                        <td>Pending</td>
+                        <td>---</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+    <!-- footer -->
+    <?php include("./common/footer.php"); ?>
+    <!-- footer -->
+
+</body>
+
+</html>
+
+<!-- offcanvas -->
+<!-- <button class="btn btn-primary d-lg-none" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasResponsive" aria-controls="offcanvasResponsive">
                     <span class="navbar-toggler-icon"></span>
                 </button>
 
@@ -61,84 +141,4 @@
                         <p class="mb-0">This is content within an <code>.offcanvas-lg</code>.</p>
                     </div>
                 </div> -->
-                </div>
-                <div>
-                    <table class="table">
-                        <thead>
-                            <tr class="title">
-                                <th scope="col">No</th>
-                                <th scope="col">Name</th>
-                                <th scope="col">Doctor</th>
-                                <th scope="col">Date&Time</th>
-                                <th scope="col">Details</th>
-                                <th scope="col">Status</th>
-                                <th scope="col">QR</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Dr.John</td>
-                                <td>9AM-11AM(SUN)</td>
-                                <td><button class="btnview">View</button></td>
-                                <td>Approved</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Mark</td>
-                                <td>Dr.John</td>
-                                <td>9AM-11AM(SUN)</td>
-                                <td><button class="btnview">View</button></td>
-                                <td>Approved</td>
-                                <td></td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Mark</td>
-                                <td>Dr.John</td>
-                                <td>9AM-11AM(SUN)</td>
-                                <td><button class="btnview">View</button></td>
-                                <td>Canceled</td>
-                                <td>---</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">1</th>
-                                <td>Mark</td>
-                                <td>Dr.John</td>
-                                <td>9AM-11AM(SUN)</td>
-                                <td><button class="btnview">View</button></td>
-                                <td>Canceled</td>
-                                <td>---</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">2</th>
-                                <td>Mark</td>
-                                <td>Dr.John</td>
-                                <td>9AM-11AM(SUN)</td>
-                                <td><button class="btnview">View</button></td>
-                                <td>Pending</td>
-                                <td>---</td>
-                            </tr>
-                            <tr>
-                                <th scope="row">3</th>
-                                <td>Mark</td>
-                                <td>Dr.John</td>
-                                <td>9AM-11AM(SUN)</td>
-                                <td><button class="btnview">View</button></td>
-                                <td>Pending</td>
-                                <td>---</td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-
-            <!-- footer -->
-            <?php include("./common/footer.php"); ?>
-            <!-- footer -->
-
-</body>
-
-</html>
+<!-- </div>
