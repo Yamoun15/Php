6ca8e48586f372d73ad378696757d2d06faae97b
LWINNANDAROO\Dell <lwinnandaroo21@gmail.com>
2022-10-10 10:04:22 +0630

    nandar edit commit

--- Project Management/04.開発/forest_healthcare/admin/View/adminHospitalAdd.php
+++ Project Management/04.開発/forest_healthcare/admin/View/adminHospitalAdd.php
@@ -9,6 +9,7 @@
     <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> -->
     <link rel="stylesheet" href="./resources/css/adminContact.css?v="<?=time()?>>
+    <link rel="stylesheet" href="./resources/css/root.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -16,19 +17,35 @@
 <!-- Bootstrap -->
 <link href="./resources/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
     <!-- FontAwesome 4.3.0 -->
-    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <!-- <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" /> -->
+    <!-- <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css"> -->
+    <script src="https://kit.fontawesome.com/5053738b7f.js" crossorigin="anonymous"></script>
     <!-- Ionicons 2.0.0 -->
     <link href="http://code.ionicframework.com/ionicons/2.0.0/css/ionicons.min.css" rel="stylesheet" type="text/css" />
     <!-- Theme style -->
     <link href="./resources/css/Admin.min.css" rel="stylesheet" type="text/css" />
+    <link href="./resources/css/_all-skins.min.css" rel="stylesheet" type="text/css" />
 </head>
 
 <body class="skin-blue">
 <!-- Header -->
 <?php include("common/header.php"); ?>
+    <section class="content-header">
+        <i class="fa fa-dashboard icon"></i>
+        <span>
+            <span class="content-header-text1">Hospital Add</span>
+            <p class="content-header-text2"><small>Hospital Add Features</small></p>
+        </span>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Hospital Add</li>
+        </ol>
+    </section>
+    <!-- Header -->
 
     <div class="container">
-    <div class="adminHospitaladdTitle">Hospital Add</div>
+        <br>
+        <br>
         <div class="row">
             <div class="col-md-3 col-sm-2 col-2"></div>
             <div class="col-md-6  col-sm-8 col-8">
@@ -67,7 +84,7 @@
                         <div class="row">
                             <div class="col-md-9"></div>
                             <div class="col-md-3">
-                                <input type="button" value="Submit" class="adminContactbtn">
+                                <input type="button" value="Submit" class="adminContactbtn hospitalBtn">
                             </div>
                         </div>
                     </div>
