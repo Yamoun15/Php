d649269b0ea9459e5ef181e5878a7c249065238b
Caroline834 <savemyanmar063@gmail.com>
2022-10-31 22:50:46 +0630

    no message

--- Project Management/04.開発/forest_healthcare/patient/View/apptHistory.php
+++ Project Management/04.開発/forest_healthcare/patient/View/apptHistory.php
@@ -83,7 +83,7 @@ include "./resources/lib/phpqrcode/qrlib.php"
                         </div>
                         <div class="modal-footer mx-auto">
                             <button type="button" class="btn btn-danger btnCancel me-3" data-bs-dismiss="modal">Cancel</button>
-                            <button type="button" class="btn btnLogOut"><a href="../Controller/logOutC.php" class="logOut">Log Out</a></button>
+                            <a href="../Controller/logOutC.php" type="button" class="btn btnLogOut ">Log Out</a>
                         </div>
                     </div>
                 </div>
