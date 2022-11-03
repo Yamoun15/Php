d696b02ade640fae3fe0634a9ebf3f727536bea9
LWINNANDAROO\Dell <lwinnandaroo21@gmail.com>
2022-11-03 16:06:39 +0630

    Nandar Final commit

--- Project Management/04.開発/forest_healthcare/patient/View/patientDetail.php
+++ Project Management/04.開発/forest_healthcare/patient/View/patientDetail.php
@@ -22,7 +22,7 @@ include "../Controller/appHistoryController.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+    <title>Patient Detail</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Inria+Serif:ital,wght@0,700;1,300&display=swap" rel="stylesheet">
